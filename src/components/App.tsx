2228d95a713ad5b4148e83832f79081dce1a47f4
Nicholas.Arocho <nick@boldlyfine.com>
2020-12-07 15:50:44 -0800

    wrap app in Apollo client

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloProvider
+} from '@apollo/client'
+
 import Header from './Header';
 import TodoPrivateWrapper from './Todo/TodoPrivateWrapper';
 import TodoPublicWrapper from './Todo/TodoPublicWrapper';
@@ -7,30 +14,45 @@ import OnlineUsersWrapper from './OnlineUsers/OnlineUsersWrapper';
 
 import { useAuth0 } from "./Auth/react-auth0-spa";
 
+const createApolloClient = (authToken: string) => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: 'https://hasura.io/learn/graphql',
+      headers: {
+        Authorization: `Bearer ${authToken}`
+      }
+    }),
+    cache: new InMemoryCache(),
+  })
+}
+
 const App = ({idToken}:{idToken:string}) => {
   const { loading, logout  } = useAuth0();
   if(loading) {
     return (<div>Loading...</div>);
   }
+  const client = createApolloClient(idToken);
   return (
-    <div>
-      <Header logoutHandler={logout} />
-      <div className="container-fluid p-left-right-0">
-        <div className="col-xs-12 col-md-9 p-left-right-0">
-          <div className="col-xs-12 col-md-6 sliderMenu p-30">
-            <TodoPrivateWrapper />
+    <ApolloProvider client={client}>
+      <div>
+        <Header logoutHandler={logout} />
+        <div className="container-fluid p-left-right-0">
+          <div className="col-xs-12 col-md-9 p-left-right-0">
+            <div className="col-xs-12 col-md-6 sliderMenu p-30">
+              <TodoPrivateWrapper />
+            </div>
+            <div className="col-xs-12 col-md-6 sliderMenu p-30 bg-gray border-right">
+              <TodoPublicWrapper />
+            </div>
           </div>
-          <div className="col-xs-12 col-md-6 sliderMenu p-30 bg-gray border-right">
-            <TodoPublicWrapper />
-          </div>
-        </div>
-        <div className="col-xs-12 col-md-3 p-left-right-0">
-          <div className="col-xs-12 col-md-12 sliderMenu p-30 bg-gray">
-            <OnlineUsersWrapper />
+          <div className="col-xs-12 col-md-3 p-left-right-0">
+            <div className="col-xs-12 col-md-12 sliderMenu p-30 bg-gray">
+              <OnlineUsersWrapper />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 };
 
